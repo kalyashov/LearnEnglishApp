284b4bf01cf10be5510a58e42d11cab9c93302a9
kalyashov <kalyashov@yandex.ru>
2020-02-24 18:29:14 +0300

    English word API - "add" method

--- src/main/java/org/learn/english/rest/EnglishWordController.java
+++ src/main/java/org/learn/english/rest/EnglishWordController.java
@@ -1,13 +1,13 @@
 package org.learn.english.rest;
 
+import org.learn.english.data.EnglishWordDAO;
 import org.learn.english.data.EnglishWordRepository;
 import org.learn.english.data.entity.EnglishWord;
 import org.learn.english.rest.data.EnglishWordResponseBuilder;
+import org.learn.english.rest.data.dto.EnglishWordDto;
 import org.learn.english.rest.data.rs.ListEnglishWordRs;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,13 +15,29 @@ import java.util.List;
  * Created by kalyashov-ga on 2020-02-05.
  */
 @RestController
-@RequestMapping("/english/word")
+@RequestMapping("/api/english/word")
 public class EnglishWordController {
+
+    private final EnglishWordDAO englishWordDAO;
+
+    @Autowired
+    public EnglishWordController(EnglishWordDAO englishWordDAO) {
+        this.englishWordDAO = englishWordDAO;
+    }
 
     @GetMapping("/random")
     public ListEnglishWordRs getRandomEnglishWords(@RequestParam(value = "count") int count) {
         EnglishWordRepository englishWordRepository = new EnglishWordRepository();
         List<EnglishWord> englishWords = englishWordRepository.getRandomWords(count);
         return EnglishWordResponseBuilder.buildEnglishWordList(englishWords);
+    }
+
+    @PostMapping("/add")
+    public void add(@RequestParam(name = "word") EnglishWordDto dto) {
+        EnglishWord englishWord = new EnglishWord();
+        englishWord.setWord(dto.getWord());
+        englishWord.setTranscription(dto.getTranslation());
+
+        englishWordDAO.save(englishWord);
     }
 }
